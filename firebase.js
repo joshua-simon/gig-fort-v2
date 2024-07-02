fd2a0b81df9dd5fc50d21e7cd8d3b0bf6b37878b
Joshua Simon <joshy.d.simon@gmail.com>
2024-07-02 19:22:56 +1200

    first commit

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDV0I98AA9wbfl79CNokx-YYJePgcH_Xw",
+  authDomain: "gig-fort-f8a59.firebaseapp.com",
+  projectId: "gig-fort-f8a59",
+  storageBucket: "gig-fort-f8a59.appspot.com",
+  messagingSenderId: "595475966846",
+  appId: "1:595475966846:web:cf94b85d69696239e5212f",
+  measurementId: "G-VD3XDEQRXH"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+export const auth = getAuth(app)
+
